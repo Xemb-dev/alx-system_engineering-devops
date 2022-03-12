7f92a79d0d46592fa44a49d63c1694af5bf25c33
Xemb-dev <olayilepraise@gmail.com>
2022-03-12 01:59:11 +0000

    prints numbers and shows their positivity status

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/** Created by Favour Okia
+ * This model runs a function to aggregate numbers 
+ * and categorize the numbers
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					if (n > 0)
+					{
+						printf("%d is positive", n);
+					}
+					else if(n == 0)
+					{
+						printf("%d is zero", n);
+					}
+					else 
+					{
+						printf("%d is negative", n);
+					}
+					return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+0-positive_or_negative prints a random number and shows if it is positive or negative
